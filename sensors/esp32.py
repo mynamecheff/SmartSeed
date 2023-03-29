e3e0ccb24c1f8614da18973dc980a73903299eea
DATmartinD <70210680+DATmartinD@users.noreply.github.com>
2023-03-29 05:05:17 +0200

    Update and rename qmtt.py to esp32.py

--- sensors/esp32.py
+++ sensors/esp32.py
@@ -5,15 +5,15 @@ from umqtt.simple2 import MQTTClient
 ssid = "Martin"
 password = "123456798"
 wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
+if not wlan.isconnected():
+    wlan.connect(ssid, password)
 
 # Set up MQTT client
-broker_ip = "192.168.173.156" # Replace with Raspberry Pi's IP address
+broker_ip = "192.168.173.156" # raspberry pi ip adresse
 port = 1883
 client_id = "esp32_client"
-topic = b"test"
-message = b"Hello from ESP32!"
+topic = "test"
+message = "Hello from ESP32!"
 
 def main(server=broker_ip, port=port):
     c = MQTTClient(client_id, server, port)
@@ -21,5 +21,4 @@ def main(server=broker_ip, port=port):
     c.publish(topic, message)
     c.disconnect()
 
-if __name__ == "__main__":
-    main()
+main()
